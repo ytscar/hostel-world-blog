a28db2d5d6469d4a679cdefd9d1104a3edbbeabe
ytscar <chigozie.orunta@hostelworld.com>
2025-08-29 12:00:04 +0100

    test: add basic unit tests

---
+++ tests/Core/ContainerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace HostelWorldBlog\Tests\Core;
+
+use Mockery;
+use WP_Mock\Tools\TestCase;
+use HostelWorldBlog\Core\Container;
+use HostelWorldBlog\Services\Admin;
+
+/**
+ * @covers \HostelWorldBlog\Core\Container::__construct
+ * @covers \HostelWorldBlog\Services\Admin::register
+ */
+class ContainerTest extends TestCase {
+	public Container $container;
+
+	public function setUp(): void {
+		\WP_Mock::setUp();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+
+	public function test_container_contains_required_services() {
+		$this->container = new Container();
+
+		$this->assertTrue( in_array( Admin::class, Container::$services, true ) );
+	}
+}
